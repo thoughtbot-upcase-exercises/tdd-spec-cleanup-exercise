ec3d3989fd2695955d9da2ddf9e74b1f2d0c3582
Chris Couzens <ccouzens@gmail.com>
2019-04-19 16:31:19 +0100

    Upgrade to Ruby 2.6
    2.3.1 is too old to easily install on rbenv on Fedora 29.
    
    I've upgraded some of the Gems because either:
    * The old versions wouldn't install on my system
    * The old versions were generating errors on my system

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -17,7 +17,7 @@ Rails.application.configure do
   config.action_mailer.perform_caching = false
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = SMTP_SETTINGS
-  config.i18n.fallbacks = true
+  config.i18n.fallbacks = [I18n.default_locale]
   config.active_support.deprecation = :notify
   config.log_formatter = ::Logger::Formatter.new
   if ENV["RAILS_LOG_TO_STDOUT"].present?
